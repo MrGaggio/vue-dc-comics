a65d1c1279f537e3ebf652b48f58221cb1c56c96
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-16 14:48:50 +0000

    Spostato il ciclo sul nuovo componente Cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,12 +1,22 @@
 <template>
   <dir>
-
+    <div class="comic">
+      <img 
+      :src="src"
+      :alt="alt">
+      <h3>{{ titolo }}</h3>
+      </div>
   </dir>
 </template>
 
 <script>
 export default {
-name: "Cards"
+name: "Cards",
+props: {
+  src: String,
+  titolo: String,
+  alt: String,
+}
 }
 </script>
 
